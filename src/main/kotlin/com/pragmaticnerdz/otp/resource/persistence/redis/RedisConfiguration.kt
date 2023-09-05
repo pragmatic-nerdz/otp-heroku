61e8e5d45955496d7e781df46602f2681af2e6d8
pragmatic-nerdz <pragmaticnerdz@gmail.com>
2023-09-05 18:39:53 -0400

    fix redis authentication

--- src/main/kotlin/com/pragmaticnerdz/otp/resource/persistence/redis/RedisConfiguration.kt
+++ src/main/kotlin/com/pragmaticnerdz/otp/resource/persistence/redis/RedisConfiguration.kt
@@ -3,6 +3,7 @@ package com.pragmaticnerdz.otp.resource.persistence.redis
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import java.net.URI
@@ -15,8 +16,19 @@ class RedisConfiguration(
     @Value("\${otp.resources.persistence.redis.url}") private val url: String,
 ) {
     @Bean
-    fun redisConnectionFactory(): LettuceConnectionFactory {
+    fun redisConnectionFactory(): LettuceConnectionFactory =
+        LettuceConnectionFactory(redisConfiguration())
+
+    private fun redisConfiguration(): org.springframework.data.redis.connection.RedisConfiguration {
         val address = URI(url)
-        return LettuceConnectionFactory(RedisStandaloneConfiguration(address.host, address.port))
+        val config = RedisStandaloneConfiguration(address.host, address.port)
+
+        val userInfo = address.userInfo
+        if (userInfo != null) {
+            val i = userInfo.indexOf(':')
+            config.username = userInfo.substring(0, i)
+            config.password = RedisPassword.of(userInfo.substring(i + 1))
+        }
+        return config
     }
 }
