109562bf78e5664f53a1fb94dc74d3d5b605dd44
pragmatic-nerdz <pragmaticnerdz@gmail.com>
2023-09-05 19:04:13 -0400

    fix mailgun config

--- src/main/kotlin/com/pragmaticnerdz/otp/resource/mail/mailgun/MailgunConfiguration.kt
+++ src/main/kotlin/com/pragmaticnerdz/otp/resource/mail/mailgun/MailgunConfiguration.kt
@@ -3,21 +3,15 @@ package com.pragmaticnerdz.otp.resource.mail.mailgun
 import com.mailgun.api.v3.MailgunMessagesApi
 import com.mailgun.client.MailgunClient
 import com.pragmaticnerdz.otp.resource.mail.EmailSenderResource
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.net.URI
 
 @Configuration
 class MailgunConfiguration(
     @Value("\${otp.resources.mail.mailgun.api-key}") private val apiKey: String,
-    @Value("\${otp.server-url}") private val serverUrl: String,
+    @Value("\${otp.resources.mail.mailgun.domain}") private val domain: String,
 ) {
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(MailgunResource::class.java)
-    }
-
     @Bean
     fun mailgunApi(): MailgunMessagesApi =
         MailgunClient
@@ -25,10 +19,6 @@ class MailgunConfiguration(
             .createApi(MailgunMessagesApi::class.java)
 
     @Bean
-    fun mailService(): EmailSenderResource {
-        val domain = URI(serverUrl).host
-        LOGGER.info("Initializing MailService with domain=$domain")
-
-        return MailgunResource(mailgunApi(), domain)
-    }
+    fun mailService(): EmailSenderResource =
+        MailgunResource(mailgunApi(), domain)
 }
