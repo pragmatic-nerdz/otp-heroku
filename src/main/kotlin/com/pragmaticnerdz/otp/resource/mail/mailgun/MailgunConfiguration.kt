7cd07c07ca9a8fca895ef0cad34e5b873243928a
pragmatic-nerdz <pragmaticnerdz@gmail.com>
2023-09-05 17:18:59 -0400

    add server url

--- src/main/kotlin/com/pragmaticnerdz/otp/resource/mail/mailgun/MailgunConfiguration.kt
+++ src/main/kotlin/com/pragmaticnerdz/otp/resource/mail/mailgun/MailgunConfiguration.kt
@@ -7,11 +7,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.net.InetAddress
+import java.net.URI
 
 @Configuration
 class MailgunConfiguration(
     @Value("\${otp.resources.mail.mailgun.api-key}") private val apiKey: String,
+    @Value("\${otp.server-url}") private val serverUrl: String,
 ) {
     companion object {
         private val LOGGER = LoggerFactory.getLogger(MailgunResource::class.java)
@@ -25,12 +26,9 @@ class MailgunConfiguration(
 
     @Bean
     fun mailService(): EmailSenderResource {
-        val domain = getDomainName()
+        val domain = URI(serverUrl).host
         LOGGER.info("Initializing MailService with domain=$domain")
 
         return MailgunResource(mailgunApi(), domain)
     }
-
-    private fun getDomainName(): String =
-        InetAddress.getLocalHost().hostName
 }
